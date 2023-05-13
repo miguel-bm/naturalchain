708fe8da09b785c3b1039ec020e06245b5f90dcd
Miguel Blanco Marcos <miguel.blanco.marcos@gmail.com>
2023-05-13 13:22:33 +0200

    update prompts

--- naturalchain/tools/smart_contract_identifier/tool.py
+++ naturalchain/tools/smart_contract_identifier/tool.py
@@ -21,31 +21,34 @@ def is_contract(address):
         return True
     else:
         return False
-    
+
 
 def is_proxy(address):
     # Convert the address to a checksum address to ensure compatibility
     checksum_address = w3.toChecksumAddress(address)
 
     # Retrieve the bytecode of the contract
-    storage_value = w3.eth.get_storage_at(checksum_address, "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc").hex()
+    storage_value = w3.eth.get_storage_at(
+        checksum_address,
+        "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc",
+    ).hex()
 
     # Check if the contract has bytecode
-    if storage_value == '0x' or int(storage_value, 16) == 0:
+    if storage_value == "0x" or int(storage_value, 16) == 0:
         return "0x"
     else:
         # Parse the storage value as an address (last 20 bytes)
-        parsed_address = '0x' + storage_value[-40:]
+        parsed_address = "0x" + storage_value[-40:]
         return parsed_address
 
 
 class TrialToolSchema(BaseModel):
-    address: str
+    address: str = Field(description="The address to the contract to be identified")
 
 
 class IdentifyTool(BaseTool):
     name = "Identify contract"
-    description = "Useful to identify a contract in the Ethereum blockchain. Return wheather is a contract or not and if it's a proxy his implementation address"
+    description = "Useful to identify a contract in the EVM blockchain. Return whether is a contract or not, and if it is a proxy, returns its implementation address"
 
     args_schema: Type[BaseModel] = TrialToolSchema
 
@@ -61,8 +64,6 @@ class IdentifyTool(BaseTool):
                 return f"The address is a proxy contract pointing to {implementationAddress}"
         else:
             return "The address is not a contract"
-       
-        
 
     async def _arun(self, address: str) -> str:
         raise NotImplementedError
