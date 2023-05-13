cb6cec42260436faf044ab29ae4db31c568d392c
invocamanman <jesus.ligero@yahoo.es>
2023-05-13 13:14:53 +0200

    identify contract tool

--- naturalchain/agents/default.py
+++ naturalchain/agents/default.py
@@ -6,6 +6,7 @@ from naturalchain.tools.calculator.tool import PythonCalculatorTool
 from naturalchain.tools.rpc.tool import RPCTool
 from naturalchain.tools.smart_contract_writer.tool import SmartContractWriterTool
 from naturalchain.tools.smart_contract_compiler.tool import SmartContractCompilerTool
+from naturalchain.tools.smart_contract_identifier.tool import IdentifyTool
 
 OPENAI_API_KEY = config("OPENAI_API_KEY")
 
@@ -16,6 +17,7 @@ naturalchain_agent = initialize_agent(
         SmartContractCompilerTool(),
         PythonCalculatorTool(),
         RPCTool(),
+        IdentifyTool()
     ],
     llm=ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY),  # type: ignore
     agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
---
+++ naturalchain/tools/smart_contract_identifier/tool.py
@@ -0,0 +1,68 @@
+from typing import Type
+
+from decouple import config
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
+from web3 import Web3
+
+INFURA_API_KEY = config("INFURA_API_KEY")
+w3 = Web3(Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{INFURA_API_KEY}"))
+
+
+def is_contract(address):
+    # Convert the address to a checksum address to ensure compatibility
+    checksum_address = w3.toChecksumAddress(address)
+
+    # Retrieve the bytecode of the contract
+    bytecode = w3.eth.getCode(checksum_address)
+
+    # Check if the contract has bytecode
+    if bytecode:
+        return True
+    else:
+        return False
+    
+
+def is_proxy(address):
+    # Convert the address to a checksum address to ensure compatibility
+    checksum_address = w3.toChecksumAddress(address)
+
+    # Retrieve the bytecode of the contract
+    storage_value = w3.eth.get_storage_at(checksum_address, "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc").hex()
+
+    # Check if the contract has bytecode
+    if storage_value == '0x' or int(storage_value, 16) == 0:
+        return "0x"
+    else:
+        # Parse the storage value as an address (last 20 bytes)
+        parsed_address = '0x' + storage_value[-40:]
+        return parsed_address
+
+
+class TrialToolSchema(BaseModel):
+    address: str
+
+
+class IdentifyTool(BaseTool):
+    name = "Identify contract"
+    description = "Useful to identify a contract in the Ethereum blockchain. Return wheather is a contract or not and if it's a proxy his implementation address"
+
+    args_schema: Type[BaseModel] = TrialToolSchema
+
+    def _run(self, address: str) -> object:
+        isContract = is_contract(address)
+
+        implementationAddress = "0x"
+        if isContract:
+            implementationAddress = is_proxy(address)
+            if implementationAddress == "0x":
+                return "The address is a contract"
+            else:
+                return f"The address is a proxy contract pointing to {implementationAddress}"
+        else:
+            return "The address is not a contract"
+       
+        
+
+    async def _arun(self, address: str) -> str:
+        raise NotImplementedError
